188bb4e25d42b134703e6ee444026dee7e967d5d
Ayhan <ayhan.mohammadi.dev@gmail.com>
2024-02-19 10:47:15 +0330

    Update Data.php

--- Data.php
+++ Data.php
@@ -1,29 +1,33 @@
-<?php 
+
+<?php
 include "MYSQL.php";
 
-$user_host      = get_current_user();
-$passhost       = "pass";  //pass host
-$database       = get_current_user()."_ayhan"; //Name data + _uaee
-$database_pass  = "ag[6-2fb-26]gfoa#js51";    //Pass data
-$domain_host    = $_SERVER['SERVER_NAME'];
+$user_host = get_current_user();
+$passhost = "pass"; 
+$database = get_current_user() . "_ayhan"; 
+$database_pass = "ag[6-2fb-26]gfoa#js51";  
+$domain_host = $_SERVER['SERVER_NAME'];
 
-$api = new cpanelAPI($user_host,$passhost,$domain_host);
+$api = new cpanelAPI($user_host, $passhost, $domain_host);
+
 $api->uapi->Mysql->create_database(
     array(
-    'name' => $database
-    )); 
-    
+        'name' => $database
+    )
+);
+
 $api->uapi->Mysql->create_user(
-   array(
-   'name'     => $database, 
-   'password' => $database_pass
-   ));
-   
+    array(
+        'name' => $database,
+        'password' => $database_pass
+    )
+);
+
 $api->uapi->Mysql->set_privileges_on_database(
     array(
-    'user'       => $database, 
-    'database'   => $database, 
-    'privileges' => 'ALL'));
-
-
-/* ayhan G.Y - dev (@HT_ayhan)
+        'user' => $database,
+        'database' => $database,
+        'privileges' => 'ALL'
+    )
+);
+?>
